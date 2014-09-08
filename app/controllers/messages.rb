24bef2697ce4ff891b6f773a88ac5037e1f0c536
& <pair+peggyfox.taylorzr@devbootcamp.com>
2014-09-08 18:37:50 -0500

    Styles showing message

--- app/controllers/messages.rb
+++ app/controllers/messages.rb
@@ -2,19 +2,21 @@ get '/messages' do
   if current_user
     @messages = current_user.received_messages.order(sent_at: :desc)
     @contacts = current_user.contacts
-  else
-    redirect '/sessions/new'
-  end
     erb :layout_sidebar do
       erb :"messages/index"
     end
+  else
+    redirect '/sessions/new'
+  end
 end
 
 get '/messages/:message_id' do
   @message = Message.find(params[:message_id])
   if current_user == @message.receiver
     @message.update(viewed_at: DateTime.now)
-    erb :"messages/show"
+    erb :layout_sidebar do
+      erb :"messages/show"
+    end
   else
     redirect '/sessions/new', layout: :non_user_layout
   end
